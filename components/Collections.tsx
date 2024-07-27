d1eea4076308cc39a041ebc34c5c0e3a73f8f268
devcowsar <kawsarcse836@gmail.com>
2024-07-27 20:22:05 +0600

    fext: getcollections_type

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -1,5 +1,27 @@
-const Collections = () => {
-  return <div>Collections</div>;
+import { getCollections } from "@/lib/actions";
+import Image from "next/image";
+import Link from "next/link";
+
+const Collections = async () => {
+  const collections = await getCollections();
+  return (
+    <div>
+      <p className="text-heading1-bold">Collections</p>
+      <div>
+        {collections.map((collection: CollectionType) => (
+          <Link href={`/collections/${collection._id}`} key={collection._id}>
+            <Image
+              src={collection.image}
+              alt={collection.title}
+              width={350}
+              height={200}
+              className="rounded-lg"
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Collections;
