6f03f985bb6d3db98320b2d629b89ef8b9045a2c
devcowsar <kawsarcse836@gmail.com>
2024-07-29 18:02:59 +0600

    fiex: productdetails

--- app/(root)/products/[productId]/page.tsx
+++ app/(root)/products/[productId]/page.tsx
@@ -1,3 +1,17 @@
-export const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+import { getProductDetailis } from "@/lib/actions";
+
+const ProductDetails = async ({
+  params,
+}: {
+  params: { productId: string };
+}) => {
+  const productDetails = await getProductDetailis(params.productId);
+  console.log(productDetails);
+  return (
+    <div className="flex justify-center items-start gap-16 py-10 px-5 max-md:flex-col max-md:items-center">
+      ProductDetails
+    </div>
+  );
 };
+
+export default ProductDetails;
--- lib/actions.ts
+++ lib/actions.ts
@@ -7,3 +7,9 @@ export const getProducts=async()=>{
     const products=await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`)
     return await products.json()
 }
+
+export const getProductDetailis=async(productId:string)=>{
+    const product=await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`)
+    return await product.json()
+
+}
