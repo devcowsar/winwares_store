b471ff39b947cf0bb0055a4e72b862271af17445
devcowsar <kawsarcse836@gmail.com>
2024-07-30 01:57:36 +0600

    fix: productinfo

--- app/(root)/products/[productId]/page.tsx
+++ app/(root)/products/[productId]/page.tsx
@@ -8,11 +8,11 @@ const ProductDetails = async ({
   params: { productId: string };
 }) => {
   const productDetails = await getProductDetailis(params.productId);
-  console.log(productDetails);
+
   return (
     <div className="flex justify-center items-start gap-16 py-10 px-5 max-md:flex-col max-md:items-center">
       <Gallery productMedia={productDetails.media} />
-      <ProductInfo />
+      <ProductInfo productInfo={productDetails} />
     </div>
   );
 };
