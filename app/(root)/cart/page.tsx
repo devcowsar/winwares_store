5aba966812935f9c0c1f224ad0c651d980c0d7ef
devcowsar <kawsarcse836@gmail.com>
2024-07-30 19:38:13 +0600

    fix: cart

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -5,8 +5,13 @@ import Image from "next/image";
 
 const Cart = () => {
   const cart = useCart();
+  const total = cart.cartItems.reduce(
+    (acc, cartItem) => acc + cartItem.item.price * cartItem.quantity,
+    0
+  );
+  const totalRounded = parseFloat(total.toFixed(2));
   return (
-    <div className="flex gap-20 py-16 px-10">
+    <div className="flex gap-20 py-16 px-10 max-lg:flex-col">
       <div className="w-2/3">
         <p className="text-body-bold">Shopping Cart</p>
         <hr className="my-6" />
@@ -15,7 +20,7 @@ const Cart = () => {
         ) : (
           <div>
             {cart.cartItems.map((cartItem) => (
-              <div className="w-full flex justify-between hover:bg-grey-1 px-6 py-5  items-center">
+              <div className="w-full flex max-sm:flex-col max-sm:gap-3 justify-between hover:bg-grey-1 px-6 py-5  items-center max-sm:items-start">
                 <div className="flex items-center">
                   <Image
                     src={cartItem.item.media[0]}
@@ -32,6 +37,7 @@ const Cart = () => {
                     {cartItem.size && (
                       <p className="text-small-medium">{cartItem.size}</p>
                     )}
+                    <p className="text-small-medium">{cartItem.item.price}</p>
                   </div>
                 </div>
                 <div className="flex gap-4 items-center">
@@ -53,6 +59,21 @@ const Cart = () => {
             ))}
           </div>
         )}
+      </div>
+      <div className="w-1/3 max-lg:w-full flex flex-col gap-8 bg-grey-1 rounded-lg px-4 py-5">
+        <p className="text-heading4-bold pb-4">
+          Summary
+          <span>{`(${cart.cartItems.length} ${
+            cart.cartItems.length > 1 ? "items" : "item"
+          })`}</span>
+        </p>
+        <div className="flex justify-between text-body-semibold">
+          <span>Totoal Amount</span>
+          <span>$ {totalRounded}</span>
+        </div>
+        <button className="border rounded-lg text-body-bold bg-white py-3 w-full hover:bg-black hover:text-white">
+          Proceed to Checkout
+        </button>
       </div>
     </div>
   );
