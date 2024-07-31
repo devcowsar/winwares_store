ec9c20332765d4708392da5ac03eba2bd7155838
devcowsar <kawsarcse836@gmail.com>
2024-08-01 02:32:44 +0600

    feat: successfulpayment

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -1,15 +1,42 @@
 "use client";
 import useCart from "@/lib/hooks/useCart";
+import { useUser } from "@clerk/nextjs";
 import { MinusCircle, PlusCircle, Trash } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const Cart = () => {
+  const router = useRouter();
+  const { user } = useUser();
   const cart = useCart();
   const total = cart.cartItems.reduce(
     (acc, cartItem) => acc + cartItem.item.price * cartItem.quantity,
     0
   );
   const totalRounded = parseFloat(total.toFixed(2));
+
+  const customer = {
+    clerkId: user?.id,
+    email: user?.emailAddresses[0].emailAddress,
+    name: user?.fullName,
+  };
+  const handleCheckout = async () => {
+    try {
+      if (!user) {
+        router.push("sign-in");
+      } else {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
+          method: "POST",
+          body: JSON.stringify({ cartItems: cart.cartItems, customer }),
+        });
+        const data = await res.json();
+        window.location.href = data.url;
+        console.log(data);
+      }
+    } catch (err) {
+      console.log("[checkout_POST]", err);
+    }
+  };
   return (
     <div className="flex gap-20 py-16 px-10 max-lg:flex-col">
       <div className="w-2/3">
@@ -20,7 +47,10 @@ const Cart = () => {
         ) : (
           <div>
             {cart.cartItems.map((cartItem) => (
-              <div className="w-full flex max-sm:flex-col max-sm:gap-3 justify-between hover:bg-grey-1 px-6 py-5  items-center max-sm:items-start">
+              <div
+                className="w-full flex max-sm:flex-col max-sm:gap-3 justify-between hover:bg-grey-1 px-6 py-5  items-center max-sm:items-start"
+                key={cartItem.item._id}
+              >
                 <div className="flex items-center">
                   <Image
                     src={cartItem.item.media[0]}
@@ -71,7 +101,10 @@ const Cart = () => {
           <span>Totoal Amount</span>
           <span>$ {totalRounded}</span>
         </div>
-        <button className="border rounded-lg text-body-bold bg-white py-3 w-full hover:bg-black hover:text-white">
+        <button
+          className="border rounded-lg text-body-bold bg-white py-3 w-full hover:bg-black hover:text-white"
+          onClick={handleCheckout}
+        >
           Proceed to Checkout
         </button>
       </div>
